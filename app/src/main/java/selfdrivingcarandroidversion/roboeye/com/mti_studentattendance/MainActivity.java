28d32da1922a15ffbd461d5041b6b0e3351a0441
DevAhmed4 <ahmedfathyr44@gmail.com>
2018-09-24 15:37:55 +0200

    Initial commit

---
+++ app/src/main/java/selfdrivingcarandroidversion/roboeye/com/mti_studentattendance/MainActivity.java
@@ -0,0 +1,13 @@
+package selfdrivingcarandroidversion.roboeye.com.mti_studentattendance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
